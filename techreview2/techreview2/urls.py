ba3c0fcdf2acd55f4b63951f57f23b76857552f5
Derek Hendrick <phantom.dev.elb@gmail.com>
2019-01-16 12:25:13 -0800

    Updates tech review and python club

--- techreview2/techreview2/urls.py
+++ techreview2/techreview2/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-urlpatterns = [
+urlpatterns=[
     path('admin/', admin.site.urls),
-    path('techapp/' include('techapp.urls')),
+    path('techapp/', include('techapp.urls')),
 ]
