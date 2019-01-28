5322d285371ce56e40c5d567e49065e29f5943fb
Derek Hendrick <phantom.dev.elb@gmail.com>
2019-01-28 11:32:00 -0800

    Updates pythonclub and techreview2
    Pythonclub is ready for assignment 5

--- techreview2/techapp/views.py
+++ techreview2/techapp/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import ProductType
 
 # Create your views here.
 def index (request):
     return render(request, 'techapp/index.html')
+
+def producttypes (request):
+    product_list = ProductType.objects.all
+    return render(request, 'techapp/types.html', {'product_list': product_list})
