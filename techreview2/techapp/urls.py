a1fd75eb88b5d3613f1a0df5006216f5f947e7c4
Derek Hendrick <phantom.dev.elb@gmail.com>
2019-02-06 11:43:46 -0800

    Updates sites for assignment 6

--- techreview2/techapp/urls.py
+++ techreview2/techapp/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns=[
     path('', views.index, name='index'),
     path('producttypes/', views.producttypes, name='types'),
+    path('getproducts/', views.getproducts, name='getproducts'),
+    path('productdetail/<int:id>', views.productdetail, name='productdetail'),
 ]
--- techreview2/techapp/views.py
+++ techreview2/techapp/views.py
@@ -1,10 +1,18 @@
-from django.shortcuts import render
-from .models import ProductType
+from django.shortcuts import render, get_object_or_404
+from .models import ProductType, Product
 
 # Create your views here.
 def index (request):
     return render(request, 'techapp/index.html')
 
 def producttypes (request):
-    product_list = ProductType.objects.all
-    return render(request, 'techapp/types.html', {'product_list': product_list})
+    type_list = ProductType.objects.all()
+    return render(request, 'techapp/types.html', {'type_list': type_list})
+
+def getproducts (request):
+    product_list = Product.objects.all()
+    return render(request, 'techapp/products.html', {'product_list': product_list})
+
+def productdetail(request, id):
+    detail=get_object_or_404(Product, pk=id)
+    return render(request, 'techapp/details.html',{'detail' : detail})
