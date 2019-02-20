2d0b5e87b5871bad663826ae29e86f3fe974e2b5
Derek Hendrick <phantom.dev.elb@gmail.com>
2019-02-20 11:38:35 -0800

    Adds stuff for assignment 8
    Still needs stuff for assignment 7

--- techreview2/techapp/urls.py
+++ techreview2/techapp/urls.py
@@ -6,4 +6,5 @@ urlpatterns=[
     path('producttypes/', views.producttypes, name='types'),
     path('getproducts/', views.getproducts, name='getproducts'),
     path('productdetail/<int:id>', views.productdetail, name='productdetail'),
+    path('newProduct/', views.newProduct, name='newproduct'),
 ]
--- techreview2/techapp/views.py
+++ techreview2/techapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import ProductType, Product
+from .forms import ProductForm
 
 # Create your views here.
 def index (request):
@@ -15,4 +16,17 @@ def getproducts (request):
 
 def productdetail(request, id):
     detail=get_object_or_404(Product, pk=id)
-    return render(request, 'techapp/details.html',{'detail' : detail})
+    return render(request, 'techapp/details.html', {'detail' : detail})
+
+#form view
+def newProduct(request):
+    form=ProductForm
+    if request.method == 'POST':
+        form = ProductForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit = True)
+            post.save()
+            form = ProductForm()
+    else:
+        form = ProductForm()
+    return render(request, 'techapp/newproduct.html', {'form': form})
